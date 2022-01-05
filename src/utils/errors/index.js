880282eb46dc6cc1e44615d1348a1a87d4c68edf
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2022-01-05 11:08:51 -0400

    move generic errors to utils layer

---
+++ src/utils/errors/index.js
@@ -0,0 +1,7 @@
+const MissingParamError = require('../../utils/errors/missing-param-error')
+const InvalidParamError = require('../../utils/errors/invalid-param-error')
+
+module.exports = {
+  MissingParamError,
+  InvalidParamError
+}
